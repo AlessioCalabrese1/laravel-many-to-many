98c51418a44c9c21a5b82c7be69f20d2c599c915
AlessioCalabrese1 <alessiocalabrese07@gmail.com>
2022-09-22 16:24:06 +0200

    Implementation of the TagsTableSeeder

--- laravel/database/seeds/DatabaseSeeder.php
+++ laravel/database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UsersTableSeeder::class,
             CategoriesSeeder::class,
+            TagsTableSeeder::class,
             PostsTableSeeder::class,
         ]);
     }
---
+++ laravel/database/seeds/TagsTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Tag;
+use Illuminate\Database\Seeder;
+
+class TagsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $tags = [
+            'sport',
+            'food',
+            'comic',
+            'html',
+            'css',
+            'laravel',
+            'js',
+        ];
+
+        foreach ($tags as $tag) {
+            $newTag = new Tag();
+            $newTag->name = $tag;
+            $newTag->save();
+        }
+    }
+}
